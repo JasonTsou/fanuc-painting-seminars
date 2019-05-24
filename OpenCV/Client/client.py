a0efc973631a7d403213f27132894828a20b9751
QQDDRR <1>
2019-05-24 17:17:12 +0300

    Almost last version
    Client commit

--- OpenCV/Client/client.py
+++ OpenCV/Client/client.py
@@ -6,7 +6,6 @@ import time
 
 start = time.time()
 
-
 def auto_canny(image, sigma=0.33):
     # compute the median of the single channel pixel intensities
     v = np.median(image)
@@ -26,45 +25,30 @@ args = ap.parse_args()
 
 image = cv2.imread(args.image_path)
 if image is not None:
+
     # доска 118*88 см
-    board_width = 1080
-    board_height = 830
+    board_width = 1000
+    board_height = 800
     width = image.shape[1]
     height = image.shape[0]
     if width > board_width:
-        width_percent = int(width * (width / board_width))
+        scale_percent = board_width/width
+        width = int(width * scale_percent)
+        height = int(height * scale_percent)
+        dim = (width, height)
+        image = cv2.resize(image, dim, interpolation=cv2.INTER_AREA)
+    if height > board_height:
+        scale_percent = board_height / height
+        width = int(width * scale_percent)
+        height = int(height * scale_percent)
+        dim = (width, height)
+        image = cv2.resize(image, dim, interpolation=cv2.INTER_AREA)
 
     gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
     blurred = cv2.GaussianBlur(gray, (3, 3), 0)
     auto = auto_canny(blurred)
-    indices = np.where(auto != [0])  # поиск координат белых пикселей
-    coordinates = set(zip(indices[1], indices[0]))
-    length = len(coordinates)
-    print(length)
-    edges = [[]]
-    i = coordinates.pop()
-    coordinates.add(i)
-    k = 0
-    while k < length:  # k - счетчик, который проверяет все ли белые точки были отнесены к какой-либо грани
-        if i in coordinates:
-            edges[len(edges) - 1].append(i)     # добовляем в edges точку и убираем ее из coordinates
-            coordinates.remove(i)
-            k += 1
-            for t in range(-1, 2):  # проверяем все точки вокруг i
-                for u in range(-1, 2):
-                    if((t != 0) or (u != 0)) and ((i[0] + t, i[1] + u) in coordinates):
-                        i = (i[0] + t, i[1] + u)
-                        break
-        else:
-            edges.append([])  # если новых белых точек вокруг i не нашлось, то отмечаем конец кривой как (-1,-1)
-            i = coordinates.pop()
-            edges[len(edges) - 1].append(i)
-            k += 1
-            for t in range(-1, 2):
-                for u in range(-1, 2):
-                    if ((t != 0) or (u != 0)) and ((i[0] + t, i[1] + u) in coordinates):
-                        i = (i[0] + t, i[1] + u)
-                        break
+    ret, thresh = cv2.threshold(auto, 127, 255, 0)
+    contours, hierarchy = cv2.findContours(thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)
     cv2.imwrite("Client/auto.png", auto)
     print(time.time() - start)
 else:
@@ -72,17 +56,58 @@ else:
 
 
 
-if(edges is not None):
-    print("Подключились")
-    # Подключаемся к серверу
-    sock = socket.socket()
-    sock.connect(('localhost', 9090))
-    for contr in edges:
-        z = 0
-        for coor in contr:
-            x = coor[0]
-            y = coor[1]
-            string = str(x) + ' ' + str(y) + ' ' + str(z)
-            sock.send(string.encode('utf-8'))
-            time.sleep(0.001)
-    sock.send('-1 -1 -1'.encode('utf-8'))
+
+print(len(contours[0]))
+
+
+# if(contours is not None):
+#     print("Подключились")
+#     # Подключаемся к серверу
+#     sock = socket.socket()
+#
+#     sock.connect(('192.168.31.150', 59002))
+#     speed = 100
+#     x = 100
+#     y = 100
+#     for i in range(len(contours)):
+#         z = -20
+#         string = '1 ' + str(x+100) + ' ' + str(y+100) + ' ' + str(z) + ' ' + str(speed) + ' 0'
+#         sock.send(string.encode('utf-8'))
+#         y = contours[i][0][0][0]
+#         x = contours[i][0][0][1]
+#         print(x)
+#         print(y)
+#         string = '1 ' + str(x+100) + ' ' + str(y+100) + ' ' + str(z) + ' ' + str(speed) + ' 0'
+#         sock.send(string.encode('utf-8'))
+#         z = -40
+#         for j in range(len(contours[i])):
+#             y = contours[i][j][0][0]
+#             x = contours[i][j][0][1]
+#             string = '1 ' + str(x+100) + ' ' + str(y+100) + ' ' + str(z) + ' ' + str(speed) + ' 0'
+#             sock.send(string.encode('utf-8'))
+#             time.sleep(0.01)
+#             print(sock.recv(1024))
+#     z = -20
+#     string = '1 ' + str(x + 100) + ' ' + str(y + 100) + ' ' + str(z) + ' ' + str(speed) + ' 0'
+#     x = 100
+#     y = 100
+#     string = '1 ' + str(x + 100) + ' ' + str(y + 100) + ' ' + str(z) + ' ' + str(speed) + ' 0'
+#     sock.close()
+
+
+# if(contours is not None):
+#     print("Подключились")
+#     # Подключаемся к серверу
+#     sock = socket.socket()
+#     sock.connect(('localhost', 9090))
+#     for i in range(len(contours)):
+#         z = -57
+#         speed = 100
+#         for coor in contours[i]:
+#             x = coor[0][0]
+#             y = coor[0][1]
+#             string = str(y) + ' ' + str(x) + ' ' + str(z)
+#             sock.send(string.encode('utf-8'))
+#             time.sleep(0.001)
+#     sock.close()
+
